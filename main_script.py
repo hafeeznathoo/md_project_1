ff2288d70f7c0724c304a2f856fea95fdfd95cff
hafeeznathoo <nathoohafeez@gmail.com>
2023-09-21 10:10:20 -0600

    scaffolding of main script

--- main_script.py
+++ main_script.py
@@ -6,3 +6,13 @@ MD proj 1 data analytics script
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+
+
+##Input
+#input_data = pd.read_csv()
+
+##Cleaning Routine
+
+
+##Initial Analysis
